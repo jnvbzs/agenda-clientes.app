b9d8d8c0371308de529135b98d7612d5d32a9c28
Jean <jeanvbonimenezes@gmail.com>
2024-05-27 16:30:48 -0400

    feat: lista de clientes deve filtrar cliente

--- src/app/pages/clientes/clientes.service.spec.ts
+++ src/app/pages/clientes/clientes.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ClientesService } from './clientes.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('ClientesService', () => {
   let service: ClientesService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      providers: [ClientesService]
+    });
     service = TestBed.inject(ClientesService);
   });
 
--- src/app/pages/clientes/lista-clientes/lista-clientes.component.spec.ts
+++ src/app/pages/clientes/lista-clientes/lista-clientes.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListaClientesComponent } from './lista-clientes.component';
+import { Cliente } from '../cliente';
+import { ClientesService } from '../clientes.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 describe('ListaClientesComponent', () => {
   let component: ListaClientesComponent;
@@ -8,16 +11,56 @@ describe('ListaClientesComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ListaClientesComponent]
-    })
-    .compileComponents();
-    
+      imports: [ListaClientesComponent, HttpClientModule],
+      providers: [ClientesService],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ListaClientesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    component.clientes = [
+      {
+        id: '4',
+        nome: 'Ana Costa',
+        email: 'ana.costa@example.com',
+        dataNascimento: '1975-07-30',
+        telefones: [
+          {
+            numero: '41987654321',
+          },
+        ],
+        documentos: [
+          {
+            tipo: 2,
+            numero: '246801357',
+          },
+        ],
+        endereco: {
+          logradouro: 'Rua do Comércio',
+          numero: '101',
+          cep: '80020000',
+          bairro: 'Mercês',
+          cidade: 'Curitiba',
+          estado: 'PR',
+          complemento: 'Apto 202',
+        },
+      },
+    ];
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('#filtrarCliente deve filtrar um ou mais clientes dado um nome', () => {
+    const nome = 'Ana';
+
+    component.filtrarCliente(nome);
+
+    const existeNomeSemCorresponderFiltro = component.clientesFiltrados?.some(
+      (cliente) => !cliente.nome.includes(nome)
+    );
+    expect(existeNomeSemCorresponderFiltro).toBeFalse();
   });
 });
