cae986adc113507f52014a5fa1b8b95e588d108b
Jean <jeanvbonimenezes@gmail.com>
2024-05-27 16:31:10 -0400

    feat: implementar listagem e filtro de clientes por nome

---
+++ src/app/pages/clientes/lista-clientes/formulario-lista-clientes.ts
@@ -0,0 +1,5 @@
+import { FormControl } from '@angular/forms';
+
+export interface FormularioListaClientes {
+  cliente: FormControl<string | null>;
+}
--- src/app/pages/clientes/lista-clientes/lista-clientes.component.ts
+++ src/app/pages/clientes/lista-clientes/lista-clientes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import {
   NgbProgressbarConfig,
   NgbProgressbarModule,
@@ -6,32 +6,88 @@ import {
 import { ClientesService } from '../clientes.service';
 import { Clientes } from '../cliente';
 import { CommonModule } from '@angular/common';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { FormularioListaClientes } from './formulario-lista-clientes';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-lista-clientes',
   standalone: true,
-  imports: [CommonModule, NgbProgressbarModule],
+  imports: [
+    CommonModule,
+    NgbProgressbarModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   providers: [NgbProgressbarConfig],
   templateUrl: './lista-clientes.component.html',
   styleUrl: './lista-clientes.component.css',
 })
-export class ListaClientesComponent implements OnInit {
+export class ListaClientesComponent implements OnInit, OnDestroy {
   public clientes?: Clientes;
+
+  public clientesFiltrados?: Clientes;
+
+  public form: FormGroup<FormularioListaClientes>;
+
+  private _inputClienteChanges?: Subscription;
 
   constructor(
     private clientesService: ClientesService,
-    progressBarConfig: NgbProgressbarConfig
+    private fb: FormBuilder,
+    private progressBarConfig: NgbProgressbarConfig
   ) {
-    progressBarConfig.max = 1000;
-    progressBarConfig.striped = true;
-    progressBarConfig.animated = true;
-    progressBarConfig.type = 'success';
-    progressBarConfig.height = '20px';
+    this.configurarProgressBar();
+
+    this.form = this.fb.group<FormularioListaClientes>({
+      cliente: this.fb.control<string | null>(null),
+    });
+
+    this._inputClienteChanges =
+      this.form.controls.cliente.valueChanges.subscribe((value) => {
+        if (value == '') this.clientesFiltrados = this.clientes;
+      });
   }
 
   ngOnInit(): void {
     this.clientesService.obterClientes().then((clientes) => {
       this.clientes = clientes;
+      this.clientesFiltrados = clientes;
     });
+  }
+
+  ngOnDestroy(): void {
+    this._inputClienteChanges?.unsubscribe();
+  }
+
+  filtrarCliente(nome: string | null) {
+    if (nome == null) this.clientesFiltrados = this.clientes;
+    else
+      this.clientesFiltrados = this.clientes?.filter((cliente) => {
+        return cliente.nome.toLowerCase().includes(nome.toLowerCase());
+      });
+  }
+
+  onInputKeyDown(event: Event) {
+    if ((event as KeyboardEvent).key == 'Enter')
+      this.filtrarCliente(this.form.controls.cliente.value);
+  }
+
+  limparFiltro() {
+    this.clientesFiltrados = this.clientes;
+    this.form.controls.cliente.reset();
+  }
+
+  private configurarProgressBar() {
+    this.progressBarConfig.max = 1000;
+    this.progressBarConfig.striped = true;
+    this.progressBarConfig.animated = true;
+    this.progressBarConfig.type = 'success';
+    this.progressBarConfig.height = '20px';
   }
 }
